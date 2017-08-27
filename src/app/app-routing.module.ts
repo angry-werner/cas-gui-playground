6103d3601fa7e9df3b93d120d4a4beea1ea39d9f
kow <werner.koch@bluewin.ch>
2017-08-27 20:26:02 +0200

    (kow) Twitter component erstellt.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TwitterFeedComponent } from './twitter-feed/twitter-feed.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', children: []},
+  { path: 'twitter', component: TwitterFeedComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TwitterFeedComponent } from './twitter-feed/twitter-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TwitterFeedComponent
   ],
   imports: [
     BrowserModule,
