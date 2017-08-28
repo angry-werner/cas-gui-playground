597ba39f70bc67d7aac79f1dd4ceeef1e6698139
kow <werner.koch@bluewin.ch>
2017-08-28 21:51:02 +0200

    (kow) Twitter feed irgendwie

--- src/app/twitter-feed/twitter-feed.component.ts
+++ src/app/twitter-feed/twitter-feed.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-twitter-feed',
   templateUrl: './twitter-feed.component.html',
   styleUrls: ['./twitter-feed.component.css']
 })
-export class TwitterFeedComponent implements OnInit {
+export class TwitterFeedComponent {
+  private webSocket: WebSocket;
+  tweets;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.tweets = [];
+    let foo = this.tweets;
+    this.webSocket = new WebSocket('ws://localhost:3001');
+    this.webSocket.onmessage = (message) => foo.unshift(JSON.parse(message.data).text);
   }
-
 }
